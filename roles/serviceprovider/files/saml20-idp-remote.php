2068036f85b89cab345947a3979538dc447224b2
fabian Strom <fs@fabianstrom.se>
2017-04-30 22:10:05 +0200

    updates

--- roles/serviceprovider/files/saml20-idp-remote.php
+++ roles/serviceprovider/files/saml20-idp-remote.php
@@ -1,6 +1,33 @@
 <?php
-// please fetch the metadata from federation page on the saml server and paste 
-// it here
-//
-// make sure that the key is the same as in (config/)authservices.php (i.e. 
-// http://auth.stacken.kth.se/saml2/idp/metadata.php)
+
+$metadata['http://auth.stacken.kth.se/saml2/idp/metadata.php'] = array (
+  'metadata-set' => 'saml20-idp-remote',
+  'entityid' => 'http://auth.stacken.kth.se/saml2/idp/metadata.php',
+  'SingleSignOnService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'http://auth.stacken.kth.se/saml2/idp/SSOService.php',
+    ),
+  ),
+  'SingleLogoutService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'http://auth.stacken.kth.se/saml2/idp/SingleLogoutService.php',
+    ),
+  ),
+  'certData' => 'MIIFnTCCA4WgAwIBAgIJANLFfj43CtEcMA0GCSqGSIb3DQEBCwUAMGUxCzAJBgNVBAYTAlNFMRIwEAYDVQQIDAlTdG9ja2hvbG0xEjAQBgNVBAcMCVN0b2NraG9sbTEQMA4GA1UECgwHU3RhY2tlbjEcMBoGA1UEAwwTYXV0aC5zdGFja2VuLmt0aC5zZTAeFw0xNzA0MTMxODUxNDFaFw0xODA0MTMxODUxNDFaMGUxCzAJBgNVBAYTAlNFMRIwEAYDVQQIDAlTdG9ja2hvbG0xEjAQBgNVBAcMCVN0b2NraG9sbTEQMA4GA1UECgwHU3RhY2tlbjEcMBoGA1UEAwwTYXV0aC5zdGFja2VuLmt0aC5zZTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAOAhkm4ZtPLrzpNaj4evVVMeBwXajO8eWHxewCkXysyCbBufU4rGuklrC7KF9+kweVEl6xcmUX56o1J/OI1N6BrA8TdjH+MymbZZXmHN3R2xMKQh/ALEJZ48iKzHpr1OniRGhjxWfKFPfOfxfZ7kisO1+d8Z1gXDyqkwkFGX5OkJGDE2PEIzzuZqmoudz3x8RcOMGZvyNPtShxUGcVzjQjFWoTKZnK2PJ9XjsuGma3NKZNYIJd1jhvcjEJQGclAsSl9elSHJfoMd2hrsSsovs/niG38WVgvG+CaoAIJpuatVESodwgVDQASa1zZDTie/GPclXyOKnkSoGQCdSLTzPC9Td6Ime3lqYIoJ8DhF+6T614IXv/Y27bwdHk8m80fiAq9xNkiftMQv3cvtg5sZn3IUVqC3mxfhy18Hu/4R+FxF8MMwBJxGjJAcwe89WJ1bKRPIM7z4kg9eEd8GXYxF5gj8XPLlFfOzYFhk0HY32tCO0Zelnc0oz7MiwhwxozQpbpdJcm9s4ks+XaoBga1/squzXjO3v7Lr235Bt/i/ZzHnuniNUuyJqrGzh8OuVxDs4YVfGTnA9IUuz3g+itfXaXEuuGkOvFnOvzWg6+h+67Rk9phHAozkIWOjrkoHaQrdQpa6jc3C7YrkpkcRsUzCp2BxhOoThAkNhGsHNa/9PNT1AgMBAAGjUDBOMB0GA1UdDgQWBBSCsbG8mp45EftxXwmR12pJ1NWRRzAfBgNVHSMEGDAWgBSCsbG8mp45EftxXwmR12pJ1NWRRzAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4ICAQA71LaOruhQ5MEr4TDTg5lql9LKnx3vwSXSsBoQOiZml8YgbNIpoUeNOpK5y5APBleetOkgg9LyWWzNwWLzPfwz+vqiQKtj5a+zPGYWs9nQdPSa3aFpcXLxW3q+JvjsiOPgwYVvq4CFZEoPLWfL6xnv49bOS+xeVD6ui0OvHkOb3AACdRZEyS4qwmMx1v6BHGIKp1uO6peo52Hupyf7h3XLni87as7D6I7kr/phlxl6aL9yy55UN8hZTZ0Tyag++jQ7URm6n5IfiHZnURHjjqx4LsB1wnczI5aXxxogIFZLCUTvNPFPsm4/dKCL3TnOhb1rE5KsrgK/Xvh4Anw2mrfbXWXnSRnh11dohoXIF5R73bfc9yKX/s6DWz7YJWpfdL+zqQqssvuquCLCtyei8Qpc8WMhEc19TgiHC6kXrlrvU7hPPvkl904eB8aztg6zWwEH5k3NKeSM3K5cx9vzzYP0hZ6b3UZ3Am+Wa+HH9gjBnyUT30KWhVbtukcXEVaYqUxMgIm4DqbvT+taf8iTGuADfIlCwcNCWmlMlAyqSN7MYQZLvn2hT2uw0ghIyDXZga1EWjcmAKoElkmFE7zIFhtqUv1SGsAg0X1k2yfZik0C1i6PZ4A7dyGjtj606STxt2fGX/PblXcYsdwypdkX6kpKUu5gRCDXoWMQaA383wi71Q==',
+  'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+  'contacts' => 
+  array (
+    0 => 
+    array (
+      'emailAddress' => 'staff@stacken.kth.se',
+      'contactType' => 'technical',
+      'givenName' => 'Administrator',
+    ),
+  ),
+);
